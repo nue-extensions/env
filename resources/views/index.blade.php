c54d6ebc6dcfef300189e8e274655a9060f5b7fd
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2022-11-09 18:59:09 +0800

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,59 +1,51 @@
-@extends('layouts.app')
-@section('title', $title)
+<script>
+    var params = '{!! request()->has('_pjax') ? '&' : '?' !!}datatable=true';
+    var table = Nue.components.NueDatatables.init('.js-datatable', {
+        scrollY: 'calc(100vh - 200px)',
+        ajax : '{!! request()->fullUrl() !!}' + params, 
+        columns: [
+            { data: 'pilihan', name: 'pilihan', className: 'pe-0 bg-light text-center', orderable: false, searchable: false },
+            { data: 'key', name: 'key' },
+			{ data: 'value', name: 'value' },
+            { data: 'action', name: 'action', className: 'text-center', orderable: false, searchable: false }, 
+        ],
+        @include('nue::partials.datatable.script')
+    });
 
-@section('js')
-	<script>
-		var table = HSCore.components.HSDatatables.init('.js-datatable', {
-			scrollY: 'calc(100vh - 251px)',
-			ajax : '{!! request()->fullUrl() !!}?datatable=true', 
-			columns: [
-				{ data: 'pilihan', name: 'pilihan', className: 'text-center', orderable: false, searchable: false },
-				{ data: 'key', name: 'key' },
-				{ data: 'value', name: 'value' },
-				{ data: 'action', name: 'action', className: 'text-end', orderable: false, searchable: false }
-			],
-			@include('nue::partials.datatable.script')
-		});
-	</script>
-@endsection
+    @include('nue::partials.datatable.delete', [
+        'url' => route("$prefix.index")
+    ])
+</script>
 
-@section('content')
-	
-	@include('nue::partials.breadcrumb', ['lists' => [
-		'Extensions' => 'javascript:;', 
-		$title => 'active'
-	]])
+@include('nue::partials.breadcrumb', ['lists' => [
+    __('Nue Extensions') => 'javascript:;', 
+    $title => 'active'
+]])
 
-	@include('nue::partials.datatable.header', [
-		'title' => $title, 
-		'description' => 'Kelola variabel umum dalam file <code>.env</code> kamu melalui halaman ini.', 
-		'create' => route("$prefix.create"), 
-		'datatable' => true
-	])
+@include('nue::partials.toolbar', [
+    'create' => route("$prefix.create"), 
+    'datatable' => true
+])
 
-	{!! Form::open(['method' => 'DELETE', 'route' => ["$prefix.destroy", 'hapus-all'], 'id' => 'submit-all']) !!}
-		<div class="card card-bordered shadow-none rounded-0">
-			<div class="card-body p-0">
-				<div class="table-responsive">
-					<table id="datatable" class="js-datatable table table-sm table-hover table-thead-bordered table-nowrap">
-						<thead class="thead-light">
-							<tr>
-								<th class="table-column-pr-0" width="1">
-									<div class="form-check mb-0">
-										<input id="datatable-checkbox-check" type="checkbox" class="form-check-input">
-										<label class="form-check-label" for="check-all"></label>
-									</div>
-								</th>
-								<th>Key</th>
-								<th>Value</th>
-								<th width="15%">Action</th>
-							</tr>
-						</thead>
-					</table>
-				</div>
-			</div>
-			@include('nue::partials.datatable.footer')
-		</div>
-	{!! Form::close() !!}
-
-@endsection
+<div class="card border-0 shadow-none rounded-0">
+    <div class="card-body p-0">
+        <div class="table-responsive">
+            <table id="datatable" class="js-datatable table table-sm table-hover table-bordered table-nowrap">
+                <thead class="thead-light">
+                    <tr>
+                        <th class="pe-0" width="1">
+                            <div class="form-check mb-0">
+                                <input id="datatable-checkbox-check" type="checkbox" class="form-check-input">
+                                <label class="form-check-label" for="check-all"></label>
+                            </div>
+                        </th>
+                        <th>{{ __('Key') }}</th>
+                        <th>{{ __('Value') }}</th>
+						<th width="1">{{ __('Action') }}</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    @include('nue::partials.datatable.footer')
+</div>
