a60075c5370c7628f43833d6194675c29fed7e3e
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2022-11-09 18:58:34 +0800

    Update edit.blade.php

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,22 +1,15 @@
-@extends('layouts.app')
-@section('title', "Edit :: $title")
+@include('nue::partials.breadcrumb', ['lists' => [
+    __('Nue Extensions') => 'javascript:;', 
+    $title => route("$prefix.index"), 
+    __('Edit') => 'active'
+]])
 
-@section('content')
+@include('nue::partials.toolbar', [
+    'back' => route("$prefix.index")
+])
 
-    @include('nue::partials.breadcrumb', ['lists' => [
-        'Extensions' => 'javascript:;', 
-        $title => route("$prefix.index"), 
-        'Edit' => 'active'
-    ]])
-
-    {!! Form::model($edit, ['route' => ["$prefix.update", $edit->id], 'method' => 'PUT']) !!}
-        <div class="card rounded-0 shadow-0 border-top-0">
-            <div class="card-header rounded-0 bg-white p-2">
-                <h2 class="page-header-title mb-0">Edit {{ $title }}</h2>
-                <p class="mb-0">Silahkan lakukan perubahan sesuai dengan kebutuhan.</p>
-            </div>
-            @include("$view.form")
-        </div>
-    {!! Form::close() !!}
-
-@endsection
+{!! Form::model($edit, ['route' => ["$prefix.update", $edit->id], 'method' => 'PUT', 'form-pjax']) !!}
+    <div class="card rounded-0 shadow-none border-0">
+        @include("$view.form")
+    </div>
+{!! Form::close() !!}
