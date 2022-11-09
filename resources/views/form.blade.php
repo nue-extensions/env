8b932d83b42dde61bd6387c7af59c5db834e555f
Noviyanto Rahmadi <noviantorahmadi@gmail.com>
2022-11-09 18:58:51 +0800

    Update form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,4 +1,4 @@
-<div class="card-body bg-light pb-10" style="min-height:calc(100vh - 163px)">
+<div class="card-body">
     <div class="row mb-3">
         <label class="col-sm-2 col-form-label" for="key">
             Key <span class="text-danger">*</span>
@@ -17,27 +17,11 @@
             {!! $errors->first('value', ' <span class="invalid-feedback">:message</span>') !!}
         </div>
     </div>
-    <div class="mb-5">&nbsp;</div>
 </div>
-<div class="position-fixed start-50 bottom-0 translate-middle-x w-100 zi-99 mb-3" style="max-width: 40rem;">
-    <div class="card card-sm bg-dark border-dark mx-2">
-        <div class="card-body">
-            <div class="row justify-content-center justify-content-sm-between">
-                <div class="col">
-                    <a href="{{ route("$prefix.index") }}" class="btn btn-ghost-light">
-                        <span class="iconify" data-icon="heroicons-solid:arrow-left"></span>
-                        Back
-                    </a>
-                </div>
-                <div class="col-auto">
-                    <div class="d-flex gap-3">
-                        <button type="reset" class="btn btn-ghost-light">Reset</button>
-                        <button type="submit" class="btn btn-primary">
-                            Save
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="card-footer bg-light border-bottom d-flex p-0">
+    <button type="reset" class="btn btn-secondary rounded-0 me-0">{{ __('Reset') }}</button>
+    <button type="submit" class="btn btn-success rounded-0">
+        <i class="bi bi-save me-1"></i>
+        {{ isset($edit) ? __('Save changes') : __('Save') }}
+    </button>
 </div>
